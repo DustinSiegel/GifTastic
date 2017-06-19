ec001989a70397479d1feef4d5b04afedbf25f9a
Dustin J. Siegel <dustinsdirectory@Dustins-MacBook-Pro.local>
2017-06-18 22:09:48 -0600

    still not working

--- assets/javaScript/app.js
+++ assets/javaScript/app.js
@@ -1,26 +1,84 @@
-var gifButtons = ["Bouldering", "Sport Climbing", "Trad Climbing", "Skate Boarding", "Mountain Biking", "Skiing", "Cliff Jumping"];
+// Array of gif topics.
+var topics = ["Bouldering", "Sport Climbing", "Trad Climbing", "Skate Boarding", "Mountain Biking", "Skiing", "Cliff Jumping"];
 
-function addButton() {
+// Global Variables
 
-  $("#gif-Adder").empty();
+// var gifSelection;
+// var buttonPress;
+var url;
+// var swapData;
+// var reSwap;
+// var rightClicked;
 
-  for (var i = 0; i < gifButtons.length; i++) {
 
-    var newButton =$("<button>");
-
+// Adds buttons to the array and displays all of the buttonss.
+function addButtons() {
+  $(".gif-Adder").empty();
+  for (var i = 0; i < topics.length; i++) {
+    var newButton = $("<button>");  
     newButton.addClass("newGif");
-    newButton.attr("data-gif", gifButtons[i]);
-    newButton.text(gifButtons[i]);
+    newButton.attr("data-gif", topics[i]);
+    newButton.text(topics[i]);
 
-    $("#gif-Adder").append(newButton);
+    $(".gif-Adder").append(newButton);
   }
 }
 
-$("#add-gif").on("click", function(event) {
+$(".add-gif").on("click", function(event) {
   event.preventDefault();
-  var gif = $("#gif-Input").val().trim();
-  gifButtons.push(gif);
-  addButton();
+  var gif = $(".gif-Input").val().trim();
+  topics.push(gif);
+  console.log("gif: " + gif);
+  addButtons();
 })
 
-addButton();
+addButtons();
+
+
+// Searches Gify.com for whatever the button pressed and produce a url.
+
+$("button").on("click", function() {
+console.log(this);
+
+  var subject = $(this).attr("data-gif");
+
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        subject + "&api_key=dc6zaTOxFJmzC&limit=10";
+    console.log("subject: " + subject);
+    console.log("url: " + queryURL);
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+
+  .done(function(response) {
+
+    var results = response.data;
+    console.log(response);
+
+    for (var i = 0; i < results.lenght; i++) {
+        var gifDiv = $("<div class='item'>");
+        var rating = results[i].rating;
+        var p = $("<p>").text("Rating: " + rating);
+        var subjectImage = $("<img>");
+        console.log("rating: " + results[i].rating);
+
+        subjectImage.attr("src", results[i].images.fixed_height.url);
+        gifDiv.append(p);
+        gifDiv.append(subjectImage);
+        console.log("gifDiv: " + gifDiv);
+        console.log("rating: " + results[i].rating);
+        $(".gif-display").prepend(gifDiv);
+
+      }
+    })
+  })
+// })
+
+
+
+
+
+
+
